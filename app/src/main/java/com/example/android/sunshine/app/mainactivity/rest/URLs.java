f7a1e271fb08fc334c92190a522c0006c395f90a
Sharmin Sultana <la4508@gmail.com>
2017-01-19 19:15:33 +0600

    Scotch vat 69

---
+++ app/src/main/java/com/example/android/sunshine/app/mainactivity/rest/URLs.java
@@ -0,0 +1,14 @@
+package com.example.android.sunshine.app.mainactivity.rest;
+
+
+/**
+ * Created by vutka bilai on 1/19/17.
+ * mail : la4508@gmail.com
+ */
+
+public class URLs {
+
+    public static final String BASE_URL = "api.openweathermap.org/data/2.5/";
+
+    public static final String Forecast_path = "forecast/daily";
+}
